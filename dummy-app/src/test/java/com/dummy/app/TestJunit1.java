d4438fe631099cbc23f22c830b5877d6521b4bd9
Hassan <hassan.shahid10@gmail.com>
2016-06-07 12:15:39 -0500

    Test 2

---
+++ dummy-app/src/test/java/com/dummy/app/TestJunit1.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+import com.dummy.app.MessageUtil;
+
+public class TestJunit1 {
+
+   String message = "Robert";
+   MessageUtil messageUtil = new MessageUtil(message);
+
+   @Test
+   public void testPrintMessage() {
+      System.out.println("Inside testPrintMessage()");
+      assertEquals(message, messageUtil.printMessage());
+   }
+}
