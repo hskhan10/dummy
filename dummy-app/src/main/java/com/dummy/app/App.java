acea12c87a944046821cdc0b4a73fad5944e5b57
Hassan <hassan.shahid10@gmail.com>
2016-06-07 11:19:13 -0500

    Init project

---
+++ dummy-app/src/main/java/com/dummy/app/App.java
@@ -0,0 +1,13 @@
+package com.dummy.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is my entry point." );
+    }
+}
