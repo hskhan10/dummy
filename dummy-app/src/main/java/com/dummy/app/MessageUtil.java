e8ae0490f5d566f7aa507e8cbcc80a85c65f4c3d
hskhan10 <hskhan10@users.noreply.github.com>
2016-06-15 17:29:32 -0500

    Hello world 1

--- dummy-app/src/main/java/com/dummy/app/MessageUtil.java
+++ dummy-app/src/main/java/com/dummy/app/MessageUtil.java
@@ -20,7 +20,7 @@ public class MessageUtil {
 
    // add "Hi!" to the message
    public String salutationMessage(){
-      message = "Hello world and goodbye! " + message;
+      message = "Hello world and goodbye11! " + message;
       System.out.println(message);
       return message;
    }
