355b5f3ed623c97b89b9f452cecf7f169609b4ed
hskhan10 <hskhan10@users.noreply.github.com>
2016-06-22 16:16:45 -0500

    test 12

--- dummy-app/src/main/java/com/dummy/app/App.java
+++ dummy-app/src/main/java/com/dummy/app/App.java
@@ -6,6 +6,6 @@ package com.dummy.app;
  */
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World! This is my sample Maven application." );
+        System.out.println( "Hello World 2! This is my sample Maven application." );
     }
 }
