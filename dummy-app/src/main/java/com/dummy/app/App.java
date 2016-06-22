16b11c9fc8e080a3c290452b4af453495bcdba09
hskhan10 <hskhan10@users.noreply.github.com>
2016-06-22 12:47:16 -0500

    Clean up

--- dummy-app/src/main/java/com/dummy/app/App.java
+++ dummy-app/src/main/java/com/dummy/app/App.java
@@ -4,10 +4,8 @@ package com.dummy.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "121Hello World! This is my entry point." );
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World! This is my sample Maven application." );
     }
 }
